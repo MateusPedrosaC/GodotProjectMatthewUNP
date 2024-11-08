c399353f7cb26180748275649200f5fb76822c6f
MateusPedrosaC <151672070+MateusPedrosaC@users.noreply.github.com>
2024-11-07 23:06:33 -0300

    Add files via upload

---
+++ Scenes/Scripts/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+var grav = 10
+var speed = 300
+var anim_sprite = AnimatedSprite2D
+
+func _ready() -> void:
+	anim_sprite = $AnimatedSprite2D 
+func _process(delta):
+	
+	if !is_on_floor():
+		velocity.y += grav
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+		$AnimatedSprite2D.flip_h = false
+	elif  Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+		$AnimatedSprite2D.flip_h = true
+	else:
+		velocity.x = 0
+	
+	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
+		velocity.y -= 300
+	
+	move_and_slide()
